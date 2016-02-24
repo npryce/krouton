c1157ec8d9b378d313308a87dfb0ae23b10d75d4
Nat Pryce <sw@natpryce.com>
2016-02-24 09:43:46 +0000

    Fix test names

--- test/com/natpryce/krouton/SuffixedRoutes.kt
+++ test/com/natpryce/krouton/SuffixedRoutes.kt
@@ -8,19 +8,19 @@ import org.junit.Test
 
 class SuffixedRoutes {
     @Test
-    fun route_prefixed_single_element() {
+    fun route_suffixed_single_element() {
         assertThat((string / "foo").parse("/bob/foo"), present(equalTo("bob")))
         assertThat((int / "bar").parse("/99/bar"), present(equalTo(99)))
     }
 
     @Test
-    fun reverse_route_for_prefixed_single_element() {
+    fun reverse_route_for_suffixed_single_element() {
         assertThat((string / "bob").path("xxx"), equalTo("/xxx/bob"))
         assertThat((int / "foo").path(72), equalTo("/72/foo"))
     }
 
     @Test
-    fun route_prefixed_single_element_when_failing() {
+    fun route_sufffixed_single_element_when_failing() {
         assertThat((int / "a").parse("/10"), absent())
         assertThat((int / "b").parse("/not-an-int/b"), absent())
         assertThat((int / "c").parse("/unwanted-prefix/10/c"), absent())
