0ae27862a899582bcec650cbfd3e7d68725dd88a
Nat Pryce <sw@natpryce.com>
2016-02-25 00:09:58 +0000

    Rename

--- test/com/natpryce/krouton/Conversion.kt
+++ test/com/natpryce/krouton/Conversion.kt
@@ -7,7 +7,7 @@ import org.junit.Test
 import java.util.*
 
 
-class Parts {
+class Conversion {
     object MillisSinceEpoch : Has1Part<Long,Date> {
         override fun fromParts(t: Long): Date? = Date(t)
         override fun toParts(u: Date) = u.time
