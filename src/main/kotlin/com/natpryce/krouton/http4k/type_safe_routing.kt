9cdcc52c643ea6d19ce993558b59c0e96d186a9e
Nat Pryce <sw@natpryce.com>
2017-09-01 19:12:36 +0100

    A first unit test for path routing in HTTP4K.

--- src/main/kotlin/com/natpryce/krouton/http4k/type_safe_routing.kt
+++ src/main/kotlin/com/natpryce/krouton/http4k/type_safe_routing.kt
@@ -2,17 +2,26 @@ package com.natpryce.krouton.http4k
 
 import com.natpryce.krouton.UrlScheme
 import com.natpryce.krouton.parse
+import com.natpryce.krouton.splitPath
 import org.http4k.core.HttpHandler
 import org.http4k.core.Request
 import org.http4k.core.Response
+import org.http4k.core.Status.Companion.NOT_FOUND
 
-typealias MatchingHttpHandler = (Request) -> Response?
 
-fun routingHttpHandler(routes: List<MatchingHttpHandler>, handlerIfNoMatch: HttpHandler): HttpHandler =
-    fun(rq: Request): Response {
-        routes.forEach { request -> request(rq)?.let { response -> return response } }
-        return handlerIfNoMatch(rq)
+private inline fun <T,U> List<T>.firstNonNull(f: (T)->U?): U? {
+    forEach { t -> f(t)?.let { return it } }
+    return null
+}
+
+typealias PathMatchingHttpHandler = (request: Request, path: List<String>) -> Response?
+
+fun pathRouter(routes: List<PathMatchingHttpHandler>, handlerIfNoMatch: HttpHandler = {Response(NOT_FOUND)}): HttpHandler =
+    fun(request: Request): Response {
+        val splitPath = splitPath(request.uri.path)
+        return routes.firstNonNull { it(request, splitPath) } ?: handlerIfNoMatch(request)
     }
 
-infix fun <T> UrlScheme<T>.by(handler: Request.(pathElements: T)-> Response): MatchingHttpHandler =
-    fun(rq: Request) = this.parse(rq.uri.path)?.let { rq.handler(it) }
+infix fun <T> UrlScheme<T>.by(handler: Request.(pathElements: T)-> Response): PathMatchingHttpHandler =
+    fun(request: Request, path: List<String>) =
+        this.parse(path)?.let { request.handler(it) }
