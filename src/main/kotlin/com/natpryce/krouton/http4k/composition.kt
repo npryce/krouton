2f514e7cff78e54459cf696b145d6823d675587c
Nat Pryce <sw@natpryce.com>
2017-11-06 11:30:27 +0000

    Move composition operators into the production code.
    Doh!  Idiot me.

--- src/main/kotlin/com/natpryce/krouton/http4k/composition.kt
+++ src/main/kotlin/com/natpryce/krouton/http4k/composition.kt
@@ -1,2 +1,8 @@
 package com.natpryce.krouton.http4k
 
+
+operator fun ResourceRouter.plus(that: ResourceRouter) =
+    ResourceRouter(this.router + that.router)
+
+operator fun <T, ROUTE: Route<T>> Router<T,ROUTE>.plus(that: Router<T,ROUTE>) =
+    Router(this.routes + that.routes, that.handlerIfNoMatch)
--- src/test/kotlin/com/natpryce/krouton/http4k/RouterCompositionTests.kt
+++ src/test/kotlin/com/natpryce/krouton/http4k/RouterCompositionTests.kt
@@ -41,9 +41,3 @@ class RouterCompositionTests {
         assertThat(composedApp.router.handlerIfNoMatch, equalTo(appY.router.handlerIfNoMatch))
     }
 }
-
-operator fun ResourceRouter.plus(that: ResourceRouter) =
-    ResourceRouter(this.router + that.router)
-
-operator fun <T, ROUTE: Route<T>> Router<T,ROUTE>.plus(that: Router<T,ROUTE>) =
-    Router(this.routes + that.routes, that.handlerIfNoMatch)
