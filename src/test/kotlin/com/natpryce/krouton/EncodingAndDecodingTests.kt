17e57da4f22849eb3fdfff49583a7f14683a79ca
Nat Pryce <sw@natpryce.com>
2018-11-10 22:21:05 +0000

    Make the EncodingAndDecodingTests more "Minutesty"

--- src/test/kotlin/com/natpryce/krouton/EncodingAndDecodingTests.kt
+++ src/test/kotlin/com/natpryce/krouton/EncodingAndDecodingTests.kt
@@ -7,14 +7,21 @@ import com.oneeyedmen.minutest.junit.context
 
 
 class EncodingAndDecodingTests : JupiterTests {
+    val examples = listOf(
+        listOf("hits", "zz top") to "/hits/zz%20top",
+        listOf("hits", "ac/dc") to "/hits/ac%2Fdc",
+        listOf("hits", "? and the mysterians") to "/hits/%3F%20and%20the%20mysterians",
+        listOf("hits", "operation: cliff clavin") to "/hits/operation%3A%20cliff%20clavin",
+        listOf("hits", "!!!") to "/hits/%21%21%21",
+        listOf("hits", "x+y") to "/hits/x%2By"
+    )
+    
     override val tests = context<Unit> {
-        test("percent encodes and decodes path elements") {
-            assertEncodeDecode(listOf("hits", "zz top"), "/hits/zz%20top")
-            assertEncodeDecode(listOf("hits", "ac/dc"), "/hits/ac%2Fdc")
-            assertEncodeDecode(listOf("hits", "? and the mysterians"), "/hits/%3F%20and%20the%20mysterians")
-            assertEncodeDecode(listOf("hits", "operation: cliff clavin"), "/hits/operation%3A%20cliff%20clavin")
-            assertEncodeDecode(listOf("hits", "!!!"), "/hits/%21%21%21")
-            assertEncodeDecode(listOf("hits", "x+y"), "/hits/x%2By")
+        examples.forEach { (elements, encoded) ->
+            test("$elements <-> $encoded") {
+                assertThat("encoding", joinPath(elements, ::encodePathElement), equalTo(encoded))
+                assertThat("decoding", splitPath(encoded), equalTo(elements))
+            }
         }
         
         test("can decode a plus if it receives one") {
@@ -22,10 +29,5 @@ class EncodingAndDecodingTests : JupiterTests {
             // actually x-www-url-form-encoded format.
             assertThat("decoding", splitPath("/hits/x+y"), equalTo(listOf("hits", "x+y")))
         }
-    }
-    
-    fun assertEncodeDecode(pathElements: List<String>, path: String) {
-        assertThat("encoding", joinPath(pathElements, ::encodePathElement), equalTo(path))
-        assertThat("decoding", splitPath(path), equalTo(pathElements))
     }
 }
